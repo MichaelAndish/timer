8494c719d85bad8263db180b1c74c8cc2e225d6c
mekaeil <maikel1370@gmail.com>
2019-06-14 04:17:17 +0430

    Create function for access to DOM elements :) and change it

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # timer
 This is a timer just for fun :) and enjoying Javascript
+
+# Usage 
+
+# About Codes :)
+
+I created function with $ we can find elements by tagName, className and ID.
+
+```
+$('span', "style.color", 'red');
+
+$('.title', "style.color", 'blue');
+
+$('#test', "style.color", 'pink');
+
+$("#test").style.color = "green";
+```
--- timer.js
+++ timer.js
@@ -1,18 +1,47 @@
 
-    $('#test').style.color = "blue";
+    // $('span', "style.color", 'blue');
+    // $('.title', "style.color", 'pink');
+    // $('#test', "style.color", 'blue');
+    // $("#test").style.color = "green";
 
-
-
-function $(value)
+function $(value, action=null, actionValue=null)
 {
-    let doms        = value.split(" ");
-    let findElementVar = null;
+    let doms            = value.split(" ");
+    let findElementVar  = null;
 
     doms.forEach(element => {
+
         findElementVar = findElement(element);
+      
+        if(findElementVar.length && action != null && actionValue != null)
+        {
+            [].forEach.call(findElementVar, function(node) 
+            {
+                setAttr(node, action, actionValue);
+            });
+
+            return;
+        }
+
+        if(action != null && actionValue != null)
+        {
+            setAttr(findElementVar, action, actionValue);
+
+            return;
+        }
+
+        return findElement;
+
     });
     
     return findElementVar;
+}
+
+function setAttr(el,actions, value)
+{
+    let arrayAction = actions.split('.');
+    el.setAttribute(arrayAction[0], (arrayAction[1]+":"+ value));
+    return;
 }
 
 function findElement(element)
